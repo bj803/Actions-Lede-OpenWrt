3863dbb54ad6c3262f0d8325b4494da5386cf1af
bj803 <109253859+bj803@users.noreply.github.com>
2024-03-07 20:09:01 +0800

    Update diy-part2.sh

--- build/803P/diy-part2.sh
+++ build/803P/diy-part2.sh
@@ -81,7 +81,7 @@ sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$qTM.tEk
 # rm -rf package/lean/luci-app-wol
 # rm -rf package/lean/luci-app-xlnetacc
 # rm -rf package/lean/luci-app-zerotier
-
+rm -rf package/lean/luci-app-mosdns
 
 
 ./scripts/feeds update -a
