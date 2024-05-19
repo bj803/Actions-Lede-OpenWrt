f4303f82a82af0faf896dc5c4164ad3c28ad6ccc
bj803 <109253859+bj803@users.noreply.github.com>
2024-05-19 14:18:41 +0800

    Update diy-part1.sh

--- build/803P2024/diy-part1.sh
+++ build/803P2024/diy-part1.sh
@@ -17,10 +17,15 @@
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 ./scripts/feeds install -a 
+
+# 编译新版Sing-box和hysteria，尽量使用golang版本1.22以上版本 ，可以用以下命令
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
