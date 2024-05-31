f38951b5e52fc83fb3ba4dec884157085f956653
bj803 <109253859+bj803@users.noreply.github.com>
2024-05-31 08:21:22 +0800

    Update diy-part2.sh

--- build/803P2024/diy-part2.sh
+++ build/803P2024/diy-part2.sh
@@ -29,16 +29,16 @@ sed -i 's/192.168.1.1/192.168.111.3/g' package/base-files/files/bin/config_gener
 #
 
 # 删除原主题
-rm -rf package/lean/luci-theme-argon
+# rm -rf package/lean/luci-theme-argon
 
 # 添加新的主题
-git clone https://github.com/kenzok8/luci-theme-ifit.git package/lean/luci-theme-ifit
+# git clone https://github.com/kenzok8/luci-theme-ifit.git package/lean/luci-theme-ifit
 
 # 取消bootstrap为默认主题
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+# sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 # 修改默认主题
-sed -i "s/luci-theme-bootstrap/luci-theme-ifit/g" feeds/luci/collections/luci/Makefile
+# sed -i "s/luci-theme-bootstrap/luci-theme-ifit/g" feeds/luci/collections/luci/Makefile
 # sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" feeds/luci/collections/luci/Makefile
 
 # 修改机器名称
