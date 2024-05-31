9c6510d0e375be897579655f57e4b2f972e92416
bj803 <109253859+bj803@users.noreply.github.com>
2024-05-31 08:36:24 +0800

    Update diy-part1.sh

--- build/803P2024/diy-part1.sh
+++ build/803P2024/diy-part1.sh
@@ -21,7 +21,7 @@ rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 ./scripts/feeds install -a 
-make menuconfig
+# make menuconfig
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
