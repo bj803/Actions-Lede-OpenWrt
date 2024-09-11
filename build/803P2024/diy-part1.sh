1df7423aa1f2fa1b3a5ebccc5e4c84fca9b21590
bj803 <109253859+bj803@users.noreply.github.com>
2024-09-11 20:27:00 +0800

    Update diy-part1.sh

--- build/803P2024/diy-part1.sh
+++ build/803P2024/diy-part1.sh
@@ -12,7 +12,6 @@
 
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall.git
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
